36bf6d25813ff9452167296c241a8c9785132a6f
oussopu <o.haffaf.dev@gmail.com>
2024-11-13 18:01:08 -0500

    fix: error message form

--- modal.js
+++ modal.js
@@ -12,19 +12,26 @@ function validate() {
   
   const firstName = document.getElementById("first")
   const firstNameInput = document.getElementById("first").value
+
   const lastName = document.getElementById("last")
   const lastNameInput = document.getElementById("last").value
+
   const email = document.getElementById("email")
   const emailInput = document.getElementById("email").value
   const regexEmail = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+")
+
   const regexDate = new RegExp("^\\d{4}-\\d{2}-\\d{2}$");
   const birthdate = document.getElementById("birthdate")
   const birthdateInput = document.getElementById("birthdate").value
+
   const quantity = document.getElementById("quantity")
   const quantityInput = document.getElementById("quantity").value
+
   const terms = document.getElementById("checkbox1")
   const checkTerms = document.getElementById("checkbox-terms")
+
   const city = document.getElementsByName("location")
+
   const firstError = document.getElementById("first-error")
   const lastError = document.getElementById("last-error")
   const emailError = document.getElementById("email-error")
@@ -32,6 +39,8 @@ function validate() {
   const quantityError = document.getElementById("quantity-error")
   const termsError = document.getElementById("terms-error")
   const cityError = document.getElementById("city-error")
+
+  let isValidated = true
 
   let citySelect = false
   for ( const radio of city){
@@ -44,7 +53,7 @@ function validate() {
   if (firstNameInput === "" || firstNameInput.length < 2) {
     firstError.style.display = "flex"
     firstName.style.border = "solid red 2px"
-    return false
+    isValidated = false
   } else {
     firstError.style.display = "none"
     firstName.style.border = ""
@@ -53,7 +62,7 @@ function validate() {
   if (lastNameInput === "" || lastNameInput.length < 2) {
     lastError.style.display = "flex"
     lastName.style.border = "solid red 2px"
-    return false
+    isValidated = false
   } else {
     lastError.style.display = "none"
     lastName.style.border = ""
@@ -62,7 +71,7 @@ function validate() {
   if (!regexEmail.test(emailInput)) {
     emailError.style.display = "flex"
     email.style.border = "solid red 2px"
-    return false
+    isValidated = false
   } else {
     emailError.style.display = "none"
     email.style.border = ""
@@ -71,7 +80,7 @@ function validate() {
   if (!regexDate.test(birthdateInput)) {
     birthdateError.style.display = "flex"
     birthdate.style.border = "solid red 2px"
-    return false
+    isValidated = false
   } else {
     birthdateError.style.display = "none"
     birthdate.style.border = ""
@@ -80,7 +89,7 @@ function validate() {
   if (quantityInput === "") {
     quantityError.style.display = "flex"
     quantity.style.border = "solid red 2px"
-    return false
+    isValidated = false
   }
   else {
     quantityError.style.display = "none"
@@ -90,7 +99,7 @@ function validate() {
   if (!terms.checked) {
     termsError.style.display = "flex"
     checkTerms.style.border = "solid red 2px"
-    return false
+    isValidated = false
   }
   else {
     termsError.style.display = "none"
@@ -99,13 +108,13 @@ function validate() {
 
   if (!citySelect) {
     cityError.style.display = "flex"
-    return false
+    isValidated = false
   }
   else {
     cityError.style.display = "none"
   }
 
-  return true
+  return isValidated
 }
 
 // DOM Elements
